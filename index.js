3f7159035058e470199cc4e6ea0ade7e2b398bb3
NoTimeInnovations <projects.notime@gmail.com>
2024-12-15 20:32:54 +0530

    added puppater

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { PORT } from "./utils/env.js";
 import { whatsapp } from "./wwjs/config.js";
+import puppeteer from "puppeteer-core";
+const chromiumPath = "/usr/bin/chromium-browser";
 
 const app = express();
-whatsapp.initialize();
+
+puppeteer
+  .launch({
+    executablePath: chromiumPath,
+    headless: true, 
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  })
+  .then((browser) => {
+    console.log("Browser launched");
+    whatsapp.initialize();
+  });
 
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -12,4 +24,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
